61972260102d7573feb28e7727c453263acf1f07
ZhannaZucher <jzucher@gmail.com>
2023-11-03 18:38:05 +0100

    adjust types

--- types.ts
+++ types.ts
@@ -101,30 +101,30 @@ export type Coin = {
   current_price: number
   market_cap: number
   market_cap_rank: number
-  fully_diluted_valuation: number
+  fully_diluted_valuation: number | null
   total_volume: number
-  high_24h: number
-  low_24h: number
-  price_change_24h: number
+  high_24h: number | null
+  low_24h: number | null
+  price_change_24h: number | null
   price_change_percentage_24h: number
-  market_cap_change_24h: number
-  market_cap_change_percentage_24h: number
+  market_cap_change_24h: number | null
+  market_cap_change_percentage_24h: number | null
   circulating_supply: number
-  total_supply: number
-  max_supply: number
+  total_supply: number | null
+  max_supply: number | null
   ath: number
   ath_change_percentage: number
-  ath_date: Date
+  ath_date: Date | string
   atl: number
   atl_change_percentage: number
-  atl_date: Date
+  atl_date: Date | string
   roi: null | ROI
-  last_updated: Date
-  price_change_percentage_14d_in_currency: number
-  price_change_percentage_1h_in_currency: number
-  price_change_percentage_1y_in_currency: number
-  price_change_percentage_200d_in_currency: number
-  price_change_percentage_24h_in_currency: number
-  price_change_percentage_30d_in_currency: number
-  price_change_percentage_7d_in_currency: number
+  last_updated: Date | string
+  price_change_percentage_14d_in_currency: number | null
+  price_change_percentage_1h_in_currency: number | null
+  price_change_percentage_1y_in_currency: number | null
+  price_change_percentage_200d_in_currency: number | null
+  price_change_percentage_24h_in_currency: number | null
+  price_change_percentage_30d_in_currency: number | null
+  price_change_percentage_7d_in_currency: number | null
 }
