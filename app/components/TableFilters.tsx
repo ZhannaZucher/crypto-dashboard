ca824b3a7786b17c00cf253264e890511f96640a
ZhannaZucher <jzucher@gmail.com>
2023-11-07 09:34:02 +0100

    toggle stableCoin filter

--- app/components/TableFilters.tsx
+++ app/components/TableFilters.tsx
@@ -1,13 +1,31 @@
+"use client"
 import Image from "next/image"
 import star from "@/public/star-full.svg"
+import { useAppDispatch } from "../store/selectors"
+import { useEffect, useState } from "react"
+import { setStableState } from "../store/stableCoinSlice"
 
 const TableFilters = () => {
+  const [showStable, setShowStable] = useState(true)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(setStableState(showStable))
+  }, [showStable, dispatch])
+
   return (
     <div className="table-filters">
       <div className="table-filters-container">
         <div className="stable-checkbox-container">
-          <input type="checkbox" id="stableCoin" defaultChecked={true} />
-          <label htmlFor="stableCoin">With stable coin</label>
+          <input
+            type="checkbox"
+            id="stableCoin"
+            defaultChecked={true}
+            onChange={() => setShowStable(!showStable)}
+          />
+          <label htmlFor="stableCoin">
+            {showStable ? "With stable coin" : "Without stable coin"}
+          </label>
         </div>
         <div className="no-list-btn">
           <p>No list</p>
